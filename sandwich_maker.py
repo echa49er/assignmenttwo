a817cd1765baad507bafcef88b8436cfffe7bcb8
echan49er <bechaniq@charlotte.edu>
2025-06-22 23:08:02 -0400

    feat: added sandwich maker module

---
+++ sandwich_maker.py
@@ -0,0 +1,15 @@
+class SandwichMaker:
+    def __init__(self, machine_resources):
+        self.machine_resources = machine_resources
+
+    def check_resources(self, ingredients):
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry there isn't enough {item}.")
+                return False
+        return True
+
+    def make_sandwich(self, sandwich_size, order_ingredients):
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
